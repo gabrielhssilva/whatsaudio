083f340c5936a19c08d7e211923365c96527a9cf
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2020-06-16 18:43:27 -0300

    feat: criação botão na página

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-alert('Funcionou');
+const interval = setInterval(()=> {
+    const header = document.querySelector(".BI4qX");
+    if (header) {
+        console.log(header);
+        clearInterval(interval);
+
+        header.classList.add("edited-header");
+
+        const button = document.createElement("button")
+        button.innerHTML = "Seguir quem te segue"
+        button.classList.add("sqdOP")
+        button.classList.add("L3NKy")
+        button.classList.add("y3zKF")
+        header.appendChild(button)
+    }
+}, 1000);
