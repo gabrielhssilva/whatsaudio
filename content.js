8154199b743e8358e0d45eccb964ab623287425c
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2020-07-26 21:11:22 -0300

    feat(init): acelerar áudio em 2x

--- content.js
+++ content.js
@@ -1,16 +1,22 @@
 const interval = setInterval(()=> {
-    const header = document.querySelector(".BI4qX");
+    const header = document.querySelector("._3euVJ");
     if (header) {
-        console.log(header);
         clearInterval(interval);
 
         header.classList.add("edited-header");
-
-        const button = document.createElement("button")
-        button.innerHTML = "Seguir quem te segue"
-        button.classList.add("sqdOP")
-        button.classList.add("L3NKy")
-        button.classList.add("y3zKF")
-        header.appendChild(button)
+        
+        const button = document.createElement("button");
+        button.innerHTML = "2x";
+        button.classList.add("style-button");
+        header.appendChild(button);
+        button.addEventListener("click", faster);
     }
 }, 1000);
+
+function faster() {
+    const audios = document.querySelectorAll("audio");
+
+    audios.forEach((audio)=>{
+        audio.playbackRate = 2;
+    });
+}
